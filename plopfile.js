9dbd1e5b6605b08c26fa3a01278b18f458e79e16
Taguchi Hayato <>
2024-10-02 10:20:36 +0900

    feature/add_plop

---
+++ plopfile.js
@@ -0,0 +1,91 @@
+module.exports = function (plop) {
+  /**
+   * コンポーネントのテンプレートジェネレータ
+   * ・コンポーネントファイル
+   * ・ストーリーファイル
+   * ・index.ts
+   */
+  plop.setGenerator('component', {
+    description: 'Generate a new component and story',
+    prompts: [
+      {
+        type: 'list',
+        name: 'level',
+        message: 'Select component level',
+        choices: ['atoms', 'molecules', 'organisms', 'templates'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name please(it will be converted to PascalCase)',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'components/{{level}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'plop-templates/component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{level}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'plop-templates/story.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{level}}/{{pascalCase name}}/index.ts',
+        templateFile: 'plop-templates/index.ts.hbs',
+      },
+    ],
+  });
+
+  /**
+   * ストーリーのテンプレートジェネレータ
+   * ・ストーリーファイル
+   */
+  plop.setGenerator('stories', {
+    description: 'Generate a new story',
+    prompts: [
+      {
+        type: 'list',
+        name: 'level',
+        message: 'Select component level',
+        choices: ['atoms', 'molecules', 'organisms', 'templates'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name please(it will be converted to PascalCase)',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'components/{{level}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'plop-templates/story.tsx.hbs',
+      },
+    ],
+  });
+
+  /**
+   * ストーリーのテンプレートジェネレータ
+   * ・ストーリーファイル
+   */
+  plop.setGenerator('stories_shadcn', {
+    description: 'Generate a new story for shadcn',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name please(for camelCase)',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'components/ui/{{name}}.stories.tsx',
+        templateFile: 'plop-templates/story.shadcn.tsx.hbs',
+      },
+    ],
+  });
+};
