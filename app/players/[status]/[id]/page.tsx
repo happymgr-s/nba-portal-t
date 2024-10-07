02d1368b46bdbd79f9a22de116b52eec94390d52
Taguchi Hayato <>
2024-10-07 12:16:00 +0900

    feature/build_error

--- app/players/[status]/[id]/page.tsx
+++ app/players/[status]/[id]/page.tsx
@@ -4,32 +4,39 @@ import { GetPlayerProfileById } from '@/app/api/nba/players/active/[id]/route';
 import { GetActivePlayersProfile } from '@/app/api/nba/players/active/route';
 
 // 静的パラメータを生成
-export async function generateStaticParams() {
-  // 選手のリストを取得する
-  const players = await axiosBase
-    .get<GetActivePlayersProfile>('/api/nba/players/active')
-    .then((res) => res.data);
+// export async function generateStaticParams() {
+//   // 選手のリストを取得する
+//   const players = await axiosBase
+//     .get<GetActivePlayersProfile>('/api/nba/players/active')
+//     .then((res) => res.data);
 
-  // 各選手の ID に基づいて静的パラメータを返す
-  return players.map((player) => {
-    return { id: player.PlayerID };
-  });
-}
+//   // 各選手の ID に基づいて静的パラメータを返す
+//   return players.map((player) => {
+//     const status = player.Status === 'Active' ? 'active' : 'freeAgent';
+//     return { status: status, id: player.PlayerID };
+//   });
+//   // return { status: 'active', id: '20000442' };
+// }
 
 /**
  * 選手詳細ページ
  */
 const PlayerByIdPage = async ({ params }: { params: { status: string; id: string } }) => {
   const { status, id } = params;
-  const profile = (await axiosBase.get<GetPlayerProfileById>(`/api/nba/players/${status}/${id}`))
-    .data;
-  return (
-    <>
-      <div>
-        {profile.FirstName} {profile.LastName} #{profile.Jersey}
-      </div>
-    </>
-  );
+
+  try {
+    const profile = (await axiosBase.get<GetPlayerProfileById>(`/api/nba/players/${status}/${id}`))
+      .data;
+    return (
+      <>
+        <div>
+          {profile.FirstName} {profile.LastName} #{profile.Jersey}
+        </div>
+      </>
+    );
+  } catch (error) {
+    return <>データを取得できませんでした。</>;
+  }
 };
 
 export default PlayerByIdPage;
