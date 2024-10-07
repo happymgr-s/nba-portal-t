2fe1886987bc47b49225e2bb9ac11d174f82fdce
Taguchi Hayato <>
2024-10-07 11:49:19 +0900

    feature/generateStaticParams

--- app/players/[status]/[id]/page.tsx
+++ app/players/[status]/[id]/page.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { axiosBase } from '@/lib/axiosBase';
 import { GetPlayerProfileById } from '@/app/api/nba/players/active/[id]/route';
+import { GetActivePlayersProfile } from '@/app/api/nba/players/active/route';
+
+// 静的パラメータを生成
+export async function generateStaticParams() {
+  // 選手のリストを取得する
+  const players = await axiosBase
+    .get<GetActivePlayersProfile>('/api/nba/players/active')
+    .then((res) => res.data);
+
+  // 各選手の ID に基づいて静的パラメータを返す
+  return players.map((player) => {
+    return { id: player.PlayerID };
+  });
+}
 
 /**
  * 選手詳細ページ
