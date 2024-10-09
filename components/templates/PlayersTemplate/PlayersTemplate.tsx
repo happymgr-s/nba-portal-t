f30b3853bf72b3b5af6f65b3c6d835a594387b79
Taguchi Hayato <>
2024-10-09 16:19:36 +0900

    feature/display_limit

--- components/templates/PlayersTemplate/PlayersTemplate.tsx
+++ components/templates/PlayersTemplate/PlayersTemplate.tsx
@@ -1,7 +1,8 @@
+'use client';
 import PlayersCard from '@/components/organisms/PlayersCard/PlayersCard';
 import PlayersSearchBar from '@/components/organisms/PlayersSearchBar/PlayersSearchBar';
 import { Player } from '@/types/player';
-import React from 'react';
+import React, { useState } from 'react';
 
 type PlayersTemplateProps = {
   players: Player[];
@@ -14,6 +15,8 @@ type PlayersTemplateProps = {
 const PlayersTemplate: React.FC<PlayersTemplateProps> = (props) => {
   const { players } = props;
 
+  const [displayLimit, setDisplayLimit] = useState(30);
+
   return (
     <>
       <div className="mb-6">
@@ -21,18 +24,31 @@ const PlayersTemplate: React.FC<PlayersTemplateProps> = (props) => {
       </div>
 
       <div className="grid grid-cols-5 gap-2">
-        {players.map((player) => {
+        {players.map((player, count) => {
           const status = player.Status === 'Active' ? 'active' : 'freeAgent';
 
           return (
-            <PlayersCard
-              key={player.PlayerID}
-              href={`/players/${status}/${player.PlayerID}`}
-              playerData={player}
-            />
+            displayLimit > count && (
+              <PlayersCard
+                key={player.PlayerID}
+                href={`/players/${status}/${player.PlayerID}`}
+                playerData={player}
+              />
+            )
           );
         })}
       </div>
+
+      {displayLimit < players.length && (
+        <div>
+          <button
+            className="text-blue-500 hover:opacity-80 active:scale-95"
+            onClick={() => setDisplayLimit((prev) => prev + 30)}
+          >
+            もっと表示する
+          </button>
+        </div>
+      )}
     </>
   );
 };
