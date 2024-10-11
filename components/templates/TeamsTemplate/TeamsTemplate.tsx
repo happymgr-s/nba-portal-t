1956c60fc9513b052fcac1685df722abc766e00e
Taguchi Hayato <>
2024-10-11 12:13:42 +0900

    fix/teams_template

--- components/templates/TeamsTemplate/TeamsTemplate.tsx
+++ components/templates/TeamsTemplate/TeamsTemplate.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import { Team } from '@/types/team';
 
-import TeamsCard from '@/components/organisms/TeamsCard/TeamsCard';
+import ConferenceTitle from '@/components/atoms/ConferenceTitle/ConferenceTitle';
+import TeamsCardsTemplate from '../TeamsCardsTemplate/TeamsCardsTemplate';
 
 type TeamsTemplateProps = {
   teams: Team[];
 };
 
 /**
- * コンポーネント
+ * チーム一覧テンプレート
  * @param props
  */
 const TeamsTemplate: React.FC<TeamsTemplateProps> = (props) => {
   const { teams } = props;
 
+  const atlanticTeams = teams.filter((team) => team.Division === 'Atlantic');
+  const centralTeams = teams.filter((team) => team.Division === 'Central');
+  const southeastTeams = teams.filter((team) => team.Division === 'Southeast');
+  const northwestTeams = teams.filter((team) => team.Division === 'Northwest');
+  const pacificTeams = teams.filter((team) => team.Division === 'Pacific');
+  const southwestTeams = teams.filter((team) => team.Division === 'Southwest');
+
   return (
     <>
-      <div className="grid grid-cols-5 gap-2">
-        {teams.map((team) => (
-          <TeamsCard key={team.Key} href={`/teams/${team.Key}`} team={team} />
-        ))}
+      <div className="flex flex-col gap-4 lg:gap-8">
+        <div className="flex flex-col gap-2 lg:gap-4">
+          <ConferenceTitle conference="Eastern" />
+
+          <TeamsCardsTemplate division="ATLANTIC" teams={atlanticTeams} />
+          <TeamsCardsTemplate division="CENTRAL" teams={centralTeams} />
+          <TeamsCardsTemplate division="SOUTHEAST" teams={southeastTeams} />
+        </div>
+
+        <div className="flex flex-col gap-2 lg:gap-4">
+          <ConferenceTitle conference="Western" />
+
+          <TeamsCardsTemplate division="NORTHWEST" teams={northwestTeams} />
+          <TeamsCardsTemplate division="PACIFIC" teams={pacificTeams} />
+          <TeamsCardsTemplate division="SOUTHWEST" teams={southwestTeams} />
+        </div>
       </div>
     </>
   );
